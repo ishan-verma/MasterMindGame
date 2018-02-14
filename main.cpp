3dac7f3d02cd04f803cbb2f00c0c645fc3921a94
ishanverma1 <ishaniv123@gmail.com>
2018-02-14 17:58:16 -0500

    New Repo for Same Proj

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MasterMindGame
+//
+//  Created by Ishan Verma on 14/02/18.
+//  Copyright © 2018 Ishan Verma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
